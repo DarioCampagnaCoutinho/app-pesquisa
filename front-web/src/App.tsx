0c11b615c60651d771c07f9e50f93dafc2d6c22d
DarioCampagnaCoutinho <campagnacoutinho67@gmail.com>
2020-09-18 08:59:14 -0300

    Componente header.

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-import Hello from './Hello';
+import Header from './components/Header';
 
 function App() {
   return (
-    <Hello message=' Campagna'/>
+    <Header />
   );
 }
 
